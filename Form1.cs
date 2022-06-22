fc1783681e19bd74791e82a7106eb01a154364f1
AATMAN(ATOMIK) <94962387+AATMAN-ATOMIK@users.noreply.github.com>
2022-06-22 12:24:59 +0530

    Form 1 creation
    Done partial design

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AatmanProject_.net_
+{
+    public partial class HomeWindow : Form
+    {
+
+    }
+}
