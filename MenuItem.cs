6541f24ee7990e41af18d176ed4aa34a86a7d32b
AATMAN(ATOMIK) <94962387+AATMAN-ATOMIK@users.noreply.github.com>
2022-06-27 11:01:42 +0530

    created menu window
    Created a window for food categories with overloaded constructor...

---
+++ MenuItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AatmanProject_.net_
+{
+    public partial class MenuItem : Form
+    {
+        public static Panel main;
+        public static HomeWindow fn;
+        int totcat,oid;
+        int pxloc = 30  , pyloc = 80 , lxloc = 100  , lyloc = 240 ;
+        
+        public MenuItem()
+        {
+            InitializeComponent();
+        }
+
+        public MenuItem(Panel p,HomeWindow f, int oi)
+        {
+            InitializeComponent();
+            main = p;
+            fn = f;
+            oid = oi;
+        }
+        
+    }
+}
