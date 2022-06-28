f4ecee21dccbba8396771c7ca641f88207269526
AATMAN(ATOMIK) <94962387+AATMAN-ATOMIK@users.noreply.github.com>
2022-06-28 11:59:11 +0530

    created a takeaway form
    This form will be a popup window that can be seen after user click on takeaway button on homewindow and this form will ask for customer details(name , number).

---
+++ takeawaydataget.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AatmanProject_.net_
+{
+    partial class takeawaydataget
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tnm = new System.Windows.Forms.TextBox();
+            this.tnum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 68);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Number :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Name    :";
+            // 
+            // tnm
+            // 
+            this.tnm.Location = new System.Drawing.Point(166, 19);
+            this.tnm.Name = "tnm";
+            this.tnm.Size = new System.Drawing.Size(175, 29);
+            this.tnm.TabIndex = 2;
+            // 
+            // tnum
+            // 
+            this.tnum.Location = new System.Drawing.Point(166, 65);
+            this.tnum.Name = "tnum";
+            this.tnum.Size = new System.Drawing.Size(175, 29);
+            this.tnum.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(166, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // takeawaydataget
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(353, 150);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tnum);
+            this.Controls.Add(this.tnm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(99)))), ((int)(((byte)(223)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "takeawaydataget";
+            this.Text = "takeawaydataget";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tnm;
+        private System.Windows.Forms.TextBox tnum;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
---
+++ takeawaydataget.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AatmanProject_.net_
+{
+    public partial class takeawaydataget : Form
+    {
+        public takeawaydataget()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tnum.Text != "" && tnum.Text != "")
+            {
+                DC.t_nm = tnum.Text;
+                DC.t_num = Convert.ToInt64(tnum.Text);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please provide info!!!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
