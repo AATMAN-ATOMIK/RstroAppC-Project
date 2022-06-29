176cb5e3bddaed7bd80290d8a3c53b87613d712a
AATMAN(ATOMIK) <94962387+AATMAN-ATOMIK@users.noreply.github.com>
2022-06-29 11:33:02 +0530

    created an  page
    Created a page for restro management

---
+++ OverViewPage.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace AatmanProject_.net_
+{
+    partial class OverViewPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OverViewPage));
+            this.p_ACF = new System.Windows.Forms.Panel();
+            this.lb_ACF = new System.Windows.Forms.Label();
+            this.pcb_ACF = new System.Windows.Forms.PictureBox();
+            this.p_INC = new System.Windows.Forms.Panel();
+            this.lb_INC = new System.Windows.Forms.Label();
+            this.pcb_INC = new System.Windows.Forms.PictureBox();
+            this.p_STM = new System.Windows.Forms.Panel();
+            this.pcb_STM = new System.Windows.Forms.PictureBox();
+            this.lb_STM = new System.Windows.Forms.Label();
+            this.p_ACF.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_ACF)).BeginInit();
+            this.p_INC.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_INC)).BeginInit();
+            this.p_STM.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_STM)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p_ACF
+            // 
+            this.p_ACF.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p_ACF.Controls.Add(this.lb_ACF);
+            this.p_ACF.Controls.Add(this.pcb_ACF);
+            this.p_ACF.Location = new System.Drawing.Point(114, 74);
+            this.p_ACF.Name = "p_ACF";
+            this.p_ACF.Size = new System.Drawing.Size(188, 223);
+            this.p_ACF.TabIndex = 0;
+            this.p_ACF.Click += new System.EventHandler(this.p_ACF_Click);
+            // 
+            // lb_ACF
+            // 
+            this.lb_ACF.AutoSize = true;
+            this.lb_ACF.Location = new System.Drawing.Point(11, 169);
+            this.lb_ACF.Name = "lb_ACF";
+            this.lb_ACF.Size = new System.Drawing.Size(166, 48);
+            this.lb_ACF.TabIndex = 1;
+            this.lb_ACF.Text = "Add Category Or\r\nFood Item\r\n";
+            this.lb_ACF.Click += new System.EventHandler(this.lb_ACF_Click);
+            // 
+            // pcb_ACF
+            // 
+            this.pcb_ACF.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pcb_ACF.Image = ((System.Drawing.Image)(resources.GetObject("pcb_ACF.Image")));
+            this.pcb_ACF.Location = new System.Drawing.Point(0, 0);
+            this.pcb_ACF.Name = "pcb_ACF";
+            this.pcb_ACF.Size = new System.Drawing.Size(186, 166);
+            this.pcb_ACF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcb_ACF.TabIndex = 0;
+            this.pcb_ACF.TabStop = false;
+            this.pcb_ACF.Click += new System.EventHandler(this.pcb_ACF_Click);
+            // 
+            // p_INC
+            // 
+            this.p_INC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p_INC.Controls.Add(this.lb_INC);
+            this.p_INC.Controls.Add(this.pcb_INC);
+            this.p_INC.Location = new System.Drawing.Point(355, 74);
+            this.p_INC.Name = "p_INC";
+            this.p_INC.Size = new System.Drawing.Size(188, 204);
+            this.p_INC.TabIndex = 1;
+            this.p_INC.Click += new System.EventHandler(this.p_INC_Click);
+            // 
+            // lb_INC
+            // 
+            this.lb_INC.AutoSize = true;
+            this.lb_INC.Location = new System.Drawing.Point(23, 169);
+            this.lb_INC.Name = "lb_INC";
+            this.lb_INC.Size = new System.Drawing.Size(135, 24);
+            this.lb_INC.TabIndex = 2;
+            this.lb_INC.Text = "Total Earning";
+            this.lb_INC.Click += new System.EventHandler(this.lb_INC_Click);
+            // 
+            // pcb_INC
+            // 
+            this.pcb_INC.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pcb_INC.Image = ((System.Drawing.Image)(resources.GetObject("pcb_INC.Image")));
+            this.pcb_INC.Location = new System.Drawing.Point(0, 0);
+            this.pcb_INC.Name = "pcb_INC";
+            this.pcb_INC.Size = new System.Drawing.Size(186, 166);
+            this.pcb_INC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcb_INC.TabIndex = 1;
+            this.pcb_INC.TabStop = false;
+            this.pcb_INC.Click += new System.EventHandler(this.pcb_INC_Click);
+            // 
+            // p_STM
+            // 
+            this.p_STM.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.p_STM.Controls.Add(this.lb_STM);
+            this.p_STM.Controls.Add(this.pcb_STM);
+            this.p_STM.Location = new System.Drawing.Point(595, 74);
+            this.p_STM.Name = "p_STM";
+            this.p_STM.Size = new System.Drawing.Size(188, 204);
+            this.p_STM.TabIndex = 2;
+            this.p_STM.Click += new System.EventHandler(this.p_STM_Click);
+            // 
+            // pcb_STM
+            // 
+            this.pcb_STM.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pcb_STM.Image = ((System.Drawing.Image)(resources.GetObject("pcb_STM.Image")));
+            this.pcb_STM.Location = new System.Drawing.Point(0, 0);
+            this.pcb_STM.Name = "pcb_STM";
+            this.pcb_STM.Size = new System.Drawing.Size(186, 166);
+            this.pcb_STM.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcb_STM.TabIndex = 1;
+            this.pcb_STM.TabStop = false;
+            this.pcb_STM.Click += new System.EventHandler(this.pcb_STM_Click);
+            // 
+            // lb_STM
+            // 
+            this.lb_STM.AutoSize = true;
+            this.lb_STM.Location = new System.Drawing.Point(6, 169);
+            this.lb_STM.Name = "lb_STM";
+            this.lb_STM.Size = new System.Drawing.Size(176, 24);
+            this.lb_STM.TabIndex = 3;
+            this.lb_STM.Text = "Staff Management";
+            this.lb_STM.Click += new System.EventHandler(this.lb_STM_Click);
+            // 
+            // OverViewPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(918, 662);
+            this.Controls.Add(this.p_STM);
+            this.Controls.Add(this.p_INC);
+            this.Controls.Add(this.p_ACF);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(99)))), ((int)(((byte)(223)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "OverViewPage";
+            this.Text = "OverViewPage";
+            this.Load += new System.EventHandler(this.OverViewPage_Load);
+            this.p_ACF.ResumeLayout(false);
+            this.p_ACF.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_ACF)).EndInit();
+            this.p_INC.ResumeLayout(false);
+            this.p_INC.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_INC)).EndInit();
+            this.p_STM.ResumeLayout(false);
+            this.p_STM.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_STM)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel p_ACF;
+        private System.Windows.Forms.Panel p_INC;
+        private System.Windows.Forms.Panel p_STM;
+        private System.Windows.Forms.PictureBox pcb_ACF;
+        private System.Windows.Forms.PictureBox pcb_INC;
+        private System.Windows.Forms.PictureBox pcb_STM;
+        private System.Windows.Forms.Label lb_ACF;
+        private System.Windows.Forms.Label lb_INC;
+        private System.Windows.Forms.Label lb_STM;
+    }
+}
---
+++ OverViewPage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AatmanProject_.net_
+{
+    public partial class OverViewPage : Form
+    {
+        
+        HomeWindow fn;
+        Panel main;
+        public OverViewPage(HomeWindow f, Panel p)
+        {
+            InitializeComponent();
+            fn = f;
+            main = p;
+        }
+
+        private void lb_ACF_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p_ACF_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pcb_ACF_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_INC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pcb_INC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p_INC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p_STM_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pcb_STM_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_STM_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OverViewPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
